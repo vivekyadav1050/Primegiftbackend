4f1b1a604c75b774db45ed8ab99ddb11a204360f
vivekyadav1050 <viveksinghald1050@gmail.com>
2026-05-03 17:21:11 +0530

    fix package config

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ dotenv.config();
 
 import app from "./app.js";
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running fine");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
